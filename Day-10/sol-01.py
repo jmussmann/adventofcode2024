6f27a3619f822d4ff3d34fa0be03ecd1720ede64
jmussmann <jmussmann+github@posteo.de>
2024-12-10 11:06:10 +0100

    change: solution day-10 python

---
+++ Day-10/sol-01.py
@@ -0,0 +1,94 @@
+import sys
+
+def parse_lines(lines):
+
+    field = []
+    for line in lines.splitlines():
+        field_line = []
+        for el in line:
+            if el == ".":
+                el = 42
+            field_line.append(int(el))
+        field.append(field_line)
+
+    return field
+
+def filter_0(line):
+    
+    res = []
+    for idx, el in enumerate(line):
+        if el == 0:
+           res.append(idx) 
+
+    return res
+
+def get_next_positions(x_pos, y_pos, next_num, field):
+
+    res = []
+
+    if x_pos - 1 > -1:
+        pos = (x_pos-1, y_pos)
+        if next_num == field[pos[1]][pos[0]]:
+            res.append(pos)
+    if y_pos - 1 > -1:
+        pos = (x_pos, y_pos-1)
+        if next_num == field[pos[1]][pos[0]]:
+            res.append(pos)
+    if x_pos + 1 < len(field[0]):
+        pos = (x_pos+1, y_pos)
+        if next_num == field[pos[1]][pos[0]]:
+            res.append(pos)
+    if y_pos + 1 < len(field):
+        pos = (x_pos, y_pos+1)
+        if next_num == field[pos[1]][pos[0]]:
+            res.append(pos)
+
+    return res
+
+def step(x_pos, y_pos, num, field):
+    
+    res = []
+    if num < 9:
+        next_num = num+1
+        next_positions = get_next_positions(x_pos, y_pos, next_num, field)
+        for pos in next_positions:
+            res += step(pos[0], pos[1], num+1, field)
+    else:
+        res = [(x_pos, y_pos)]
+
+    return res
+
+def count_unique_pos(positions):
+    
+    pos_map = {}
+    res = 0
+    for pos in positions:
+        if pos[0] not in pos_map:
+            pos_map[pos[0]] = {}
+        if pos[1] not in pos_map[pos[0]]:
+            pos_map[pos[0]][pos[1]] = 1
+            res += 1
+
+    return res
+
+def process_field(field):
+
+    res = 0
+    for y_pos, line in enumerate(field):
+        zeros = filter_0(line)
+        for zero_x_pos in zeros:
+            reachable = step(zero_x_pos, y_pos, 0, field)
+            res += count_unique_pos(reachable)
+    return res
+
+def main(argv):
+
+    with open(argv[0], "r") as fd:
+        lines = fd.read()
+
+    field = parse_lines(lines)
+
+    print("Result: ", process_field(field))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
